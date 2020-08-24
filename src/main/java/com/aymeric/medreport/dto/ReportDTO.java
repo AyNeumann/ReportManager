9a53baf7e3cca14924cf53703d93a58c71c15299
Aymeric NEUMANN <neumann.0512@apside-groupe.com>
2020-08-24 15:07:56 +0200

    ADD REPORT DTO REMOVE USELESS CODE

---
+++ src/main/java/com/aymeric/medreport/dto/ReportDTO.java
@@ -0,0 +1,167 @@
+package com.aymeric.medreport.dto;
+
+import java.util.Date;
+import java.util.List;
+
+import com.aymeric.medreport.model.Comment;
+import com.aymeric.medreport.model.Customer;
+import com.aymeric.medreport.model.Document;
+import com.aymeric.medreport.model.Domain;
+import com.aymeric.medreport.model.Status;
+
+public class ReportDTO {
+    
+    private Long id;
+
+    private Date creationDate;
+
+    private Date modificationDate;
+
+    private String title;
+
+    private Domain domain;
+
+    private Status status;
+
+    private List<Comment> comment;
+
+    private List<Document> documents;
+
+    private Customer customer;
+    
+    /**
+     * ToString override method
+     */
+    @Override
+    public String toString() {
+        return "ReportDTO [id=" + id + ", creationDate=" + creationDate + ", modificationDate=" + modificationDate
+                + ", title=" + title + ", domain=" + domain + ", status=" + status + ", comment=" + comment
+                + ", documents=" + documents + ", customer=" + customer + "]";
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the creationDate
+     */
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    /**
+     * @param creationDate the creationDate to set
+     */
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    /**
+     * @return the modificationDate
+     */
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    /**
+     * @param modificationDate the modificationDate to set
+     */
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the domain
+     */
+    public Domain getDomain() {
+        return domain;
+    }
+
+    /**
+     * @param domain the domain to set
+     */
+    public void setDomain(Domain domain) {
+        this.domain = domain;
+    }
+
+    /**
+     * @return the status
+     */
+    public Status getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the comment
+     */
+    public List<Comment> getComment() {
+        return comment;
+    }
+
+    /**
+     * @param comment the comment to set
+     */
+    public void setComment(List<Comment> comment) {
+        this.comment = comment;
+    }
+
+    /**
+     * @return the documents
+     */
+    public List<Document> getDocuments() {
+        return documents;
+    }
+
+    /**
+     * @param documents the documents to set
+     */
+    public void setDocuments(List<Document> documents) {
+        this.documents = documents;
+    }
+
+    /**
+     * @return the customer
+     */
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    /**
+     * @param customer the customer to set
+     */
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
